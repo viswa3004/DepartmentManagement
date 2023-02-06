05cd066be405a35a7205e7cb7659dbb46f766013
ViswaSneha <viswanadh.kasi44@gmail.com>
2023-02-07 00:32:15 +0530

    Department logo changes in swagger

--- src/main/java/com/departmentmanagement/DepartmentManagementApplication.java
+++ src/main/java/com/departmentmanagement/DepartmentManagementApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@OpenAPIDefinition(info = @Info(title = "Employees API", version = "2.0", description = "Employees Information"))
+@OpenAPIDefinition(info = @Info(title = "Department API", version = "2.0", description = "Department Information"))
 public class DepartmentManagementApplication {
 
 	public static void main(String[] args) {
